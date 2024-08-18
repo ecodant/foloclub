692f898042749773784cb2fb544751adfe3ba02c
ecodant <edwincodant@gmail.com>
2024-08-18 12:52:39 -0500

    base project

---
+++ src/main/java/co/uni/foloclub/foloclub/FoloclubApplication.java
@@ -0,0 +1,14 @@
+package co.uni.foloclub.foloclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoloclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoloclubApplication.class, args);
+    // System.out.println("hello mom");
+	}
+
+}
---
+++ src/test/java/co/uni/foloclub/foloclub/FoloclubApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uni.foloclub.foloclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoloclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
